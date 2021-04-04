7dee414bae662550191a5723dff6ec3eaba334e2
Adrian Pikor <loler88@hotmail.ca>
2021-04-04 15:58:38 -0700

    UI can accept multiple genres, will make an API call on each one; commented all the code to improve documentation

--- main.pl
+++ main.pl
@@ -1,43 +1,36 @@
 :- use_module(library(http/http_open)).
 :- use_module(library(http/json)).
 
-% example URL:
-% http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=rock&limit=2&api_key=5dd0808d6f467e08e647a024417e7318&format=json
+
+
+% -----CONSTANTS-----
+
 root_url("http://ws.audioscrobbler.com/2.0").
 api_key("5dd0808d6f467e08e647a024417e7318").
 api_method("tag.gettoptracks").
 
+
+
+% -----URL BUILDING FUNCTIONS-----
+
+% converts a Tag atom into the string "tag=<Tag>", for the "tag" URL parameter
 param_tag(Tag, TagParam) :-
 	string_concat("tag=", Tag, TagParam).
 
+% converts a Method atom into the string "method=<Method>", for the "method" URL parameter
 param_method(Method, MethodParam) :-
 	string_concat("method=", Method, MethodParam).
 
-param_limit(Num, LimitParam) :-
-	string_concat("limit=", Num, LimitParam).
+% converts a Limit atom into the string "limit=<Limit>", for the "limit" URL parameter
+param_limit(Limit, LimitParam) :-
+	string_concat("limit=", Limit, LimitParam).
 
+% converts a Key atom into the string "api_key=<Key>", for the "api_key" URL parameter
 param_key(Key, KeyParam) :-
 	string_concat("api_key=", Key, KeyParam).
 
-
-% adapted from https://github.com/thibaultdewit/Interactive-Tour-Guide
-make_api_call(URL, Songs) :-
- 	api_key(Key),
- 	http_open(URL, In_stream, [request_header('Authorization'=Key)]),
- 	json_read_dict(In_stream, Dict),
- 	close(In_stream),
- 	json_to_songs(Dict, Songs),
- 	traverse_dict(Songs, _).
-
-json_to_songs(Dict, Dict.tracks.track).
-
-traverse_dict([], _).
-traverse_dict([Track|T], Song) :-
-	Song = Track.name,
- 	string_concat(Song, " \n", SongNL),
- 	write(SongNL),
-  	traverse_dict(T, _).
-
+% constructs the URL to make the API call with; example URL:
+% http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=rock&limit=5&api_key=5dd0808d6f467e08e647a024417e7318&format=json
 build_url(Tag, URL) :-
 	root_url(Root),
 	api_key(Key),
@@ -58,18 +51,52 @@ build_url(Tag, URL) :-
 	string_concat(R9, "format=json", URL).
 	%% write(URL).
 
-call_api(Input) :-
-	build_url(Input, URL),
+
+
+% -----API CALLER AND JSON HANDLERS-----
+
+% converts dictionary Dict that json_read_dict() (in make_api_call()) returns
+% into the list of songs the JSON contains
+json_to_songs(Dict, Dict.tracks.track).
+
+% writes the "name" field of each Track to the console
+write_songnames([]).
+write_songnames([Track|T]) :-
+	Song = Track.name,
+ 	string_concat(Song, " \n", SongNL),
+ 	write(SongNL),
+  	traverse_dict(T).
+
+% makes the HTTP get request to lastFM's API
+% Songs is a list of Song atoms (Song is an atom representing a song name)
+% CITATION: adapted from https://github.com/thibaultdewit/Interactive-Tour-Guide
+make_api_call(URL, Songs) :-
+ 	http_open(URL, In_stream, []),
+ 	json_read_dict(In_stream, Dict),
+ 	close(In_stream),
+ 	json_to_songs(Dict, Songs),
+ 	write_songnames(Songs),
+ 	write("\n").
+
+% builds the URL and makes the API call for this particular Genre
+call_api_single(Genre) :-
+	atom_string(Genre, GenreStr),
+	build_url(GenreStr, URL),
 	make_api_call(URL, _).
 	%% write(Songs).
 
+% calls the api via call_api_single() for each Genre in a list of Genre's
+call_api([]).
+call_api([Genre|T]) :-
+	call_api_single(Genre),
+	call_api(T).
+	
 
 
-
-
-
+% -----USER INTERFACE-----
 q :-
     write("Genre? (space-separated list) "), flush_output(current_output),
     readln(Input),
-    atomics_to_string(Input, InputStr),
-    call_api(InputStr).
+    %% write(Input),
+    %% atomics_to_string(Input, InputStr),
+    call_api(Input).
