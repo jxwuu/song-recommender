05465952d8ab687a2c7b18342b5b9b8c362727f3
Victor Cheng <victorcheng0425@gmail.com>
2021-04-12 02:13:25 -0700

    Victor

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-# song-recommender
-prolog project! 
+# Song-Recommender
+Prolog project
 
 With the amount of songs being produced to date, it may be difficult and overwhelming to find new music that users would like to listen to. To aid in this problem, our Prolog recommendation system will be able to produce a personalized playlist with the users input being their preference in music.
+
+# Instruction
+-Clone the repo, type [main]. to execute the program
+-type mainUI. to start the program
+-Follow the instruction and you are good to go!
+
+# Notes
+1. Our Prolog recommendation system has three main features. 1. Top Track, 2. Top Album, 3. Top Artist 
+2. You can select genre/ artist/ year in one of the features,  and the system will return the top 5 songs of that category!
+3. You can select "General User Interface" which guide you throughout the program, or "Natural Language User Interface" that you can ask any questions you want, our system will analyze your question and give you the feedback!
--- main.pl
+++ main.pl
@@ -7,9 +7,12 @@
 
 root_url("http://ws.audioscrobbler.com/2.0").
 api_key("5dd0808d6f467e08e647a024417e7318").
-api_method("tag.gettoptracks").
+api_method_toptrack("tag.gettoptracks").
+api_method_topartists("tag.gettopartists").
+api_method_albums("tag.gettopalbums").
 possible_tagArray([genre, artist, year]).
-
+ui_tag([g,n]).
+tag_option([track,artist,album]).
 
 
 % -----URL BUILDING FUNCTIONS-----
@@ -32,13 +35,57 @@ param_key(Key, KeyParam) :-
 
 % constructs the URL to make the API call with; example URL:
 % http://ws.audioscrobbler.com/2.0/?method=tag.gettoptracks&tag=rock&limit=5&api_key=5dd0808d6f467e08e647a024417e7318&format=json
-build_url(Tag, URL) :-
+build_url_toptracks(Tag, URL) :-
 	root_url(Root),
 	api_key(Key),
-	api_method(Method),
+	api_method_toptrack(Method),
 	param_tag(Tag, TagParam),
 	param_method(Method, MethodParam),
-	param_limit(10, LimitParam),
+	param_limit(5, LimitParam),
+	param_key(Key, KeyParam),
+	string_concat(Root, "?", R1),
+	string_concat(R1, MethodParam, R2),
+	string_concat(R2, "&", R3),
+	string_concat(R3, TagParam, R4),
+	string_concat(R4, "&", R5),
+	string_concat(R5, LimitParam, R6),
+	string_concat(R6, "&", R7),
+	string_concat(R7, KeyParam, R8),
+	string_concat(R8, "&", R9),
+	string_concat(R9, "format=json", URL).
+	%% write(URL).
+
+% constructs the URL to make the API call with; example URL:
+% http://ws.audioscrobbler.com/2.0/?method=tag.gettopartists&tag=rock&limit=5&api_key=5dd0808d6f467e08e647a024417e7318&format=json
+build_url_topartists(Tag, URL) :-
+	root_url(Root),
+	api_key(Key),
+	api_method_topartists(Method),
+	param_tag(Tag, TagParam),
+	param_method(Method, MethodParam),
+	param_limit(5, LimitParam),
+	param_key(Key, KeyParam),
+	string_concat(Root, "?", R1),
+	string_concat(R1, MethodParam, R2),
+	string_concat(R2, "&", R3),
+	string_concat(R3, TagParam, R4),
+	string_concat(R4, "&", R5),
+	string_concat(R5, LimitParam, R6),
+	string_concat(R6, "&", R7),
+	string_concat(R7, KeyParam, R8),
+	string_concat(R8, "&", R9),
+	string_concat(R9, "format=json", URL).
+	%% write(URL).
+
+	% constructs the URL to make the API call with; example URL:
+% http://ws.audioscrobbler.com/2.0/?method=tag.gettopalbums&tag=rock&limit=5&api_key=5dd0808d6f467e08e647a024417e7318&format=json
+build_url_topalbums(Tag, URL) :-
+	root_url(Root),
+	api_key(Key),
+	api_method_albums(Method),
+	param_tag(Tag, TagParam),
+	param_method(Method, MethodParam),
+	param_limit(5, LimitParam),
 	param_key(Key, KeyParam),
 	string_concat(Root, "?", R1),
 	string_concat(R1, MethodParam, R2),
@@ -56,11 +103,14 @@ build_url(Tag, URL) :-
 
 % converts dictionary Dict that json_read_dict() (in make_api_call()) returns
 % into the list of songs the JSON contains
-json_to_songs(Dict, Dict.tracks.track).
+json_to_songs_track(Dict, Dict.tracks.track).
+json_to_songs_artist(Dict, Dict.topartists.artist).
+json_to_songs_alblum(Dict, Dict.albums.album).
 
 % writes the "name" field of each Track to the console
-write_songnames([]).
-write_songnames([Track|T]) :-
+% Handle Top track and top album
+write_songnames_track([]).
+write_songnames_track([Track|T]) :-
 	Song_name = Track.name,
  	Song_artist = Track.artist.name,
 	Song_url = Track.url,
@@ -72,24 +122,70 @@ write_songnames([Track|T]) :-
 	string_concat(Song_artist_name2, Song_url, Song_artist_name3),  
  	string_concat(Song_artist_name3, "\n\n", Song_final),
  	write(Song_final),
-  	write_songnames(T).
+  	write_songnames_track(T).
+
+% Handle Top artist
+write_songnames_artist([]).
+write_songnames_artist([Track|T]) :-
+	Song_name = Track.name,
+ 	%Song_artist = Track.artist.name,
+	Song_url = Track.url,
+	string_concat("Top Artist", ": ", Artist),
+	string_concat(Artist, Song_name, Artist_name),
+	string_concat(Artist_name, "\nLink: ", Song_artist_name2),
+	string_concat(Song_artist_name2, Song_url, Song_artist_name3),  
+ 	string_concat(Song_artist_name3, "\n\n", Song_final),
+ 	write(Song_final),
+  	write_songnames_artist(T).
 
 % makes the HTTP get request to lastFMs API
 % Songs is a list of Song atoms (Song is an atom representing a song name)
 % CITATION: adapted from https://github.com/thibaultdewit/Interactive-Tour-Guide
-make_api_call(URL, Songs) :-
+make_api_call_track(URL, Songs) :-
  	http_open(URL, In_stream, []),
  	json_read_dict(In_stream, Dict),
  	close(In_stream),
- 	json_to_songs(Dict, Songs),
-	write_songnames(Songs),
+ 	json_to_songs_track(Dict, Songs),
+	write_songnames_track(Songs),
+ 	write("\n").
+
+make_api_call_artist(URL, Songs) :-
+ 	http_open(URL, In_stream, []),
+ 	json_read_dict(In_stream, Dict),
+ 	close(In_stream),
+ 	json_to_songs_artist(Dict, Songs),
+	write_songnames_artist(Songs),
+ 	write("\n").
+
+make_api_call_alblum(URL, Songs) :-
+ 	http_open(URL, In_stream, []),
+ 	json_read_dict(In_stream, Dict),
+ 	close(In_stream),
+ 	json_to_songs_alblum(Dict, Songs),
+	write_songnames_track(Songs),
  	write("\n").
 
 % builds the URL and makes the API call for this particular Item
-call_api_single(Item) :-
+% For top track
+call_api_single_track(Item) :-
+	write("Building Track URL........\n"),
 	%% atom_string(Item, ItemStr),
-	build_url(Item, URL),
-	make_api_call(URL, _).
+	build_url_toptracks(Item, URL),
+	make_api_call_track(URL, _).
+
+% For top artist
+call_api_single_artist(Item) :-
+	write("Building Artist URL........\n"),
+	%% atom_string(Item, ItemStr),
+	build_url_topartists(Item, URL),
+	make_api_call_artist(URL, _).
+
+% For top alblum
+call_api_single_alblum(Item) :-
+	write("Building Alblum URL........\n"),
+	%% atom_string(Item, ItemStr),
+	build_url_topalbums(Item, URL),
+	make_api_call_alblum(URL, _).
 
 % calls the api via call_api_single() for each Item in a list of Items
 call_api([]).
@@ -104,6 +200,16 @@ isNumber([X]) :-
 % verfiies if input is genre or artist or year
 isGenreOrArtist([In]) :-
 	possible_tagArray(P),
+	member(In,P).
+
+% verfiies if input is g or n (check UI)
+isNaturalOrSimple([In]) :-
+	ui_tag(P),
+	member(In,P).
+
+% verfiies if input is 1 or 2 or 3
+isTrackOrArtistOrAlbum([In]) :-
+	tag_option(P),
 	member(In,P).
 
 %% % verifies if Input is the atom 'genre'
@@ -123,64 +229,297 @@ isGenreOrArtist([In]) :-
 %% 	Songs = [].
 
 % if input is genre then computes genre playlist
-genreOrArtist([Input]) :-
+genreOrArtistTrack([Input]) :-
 	Input = 'genre' ->
-	chooseGenre
+	chooseGenreTrack
 ;	Input = 'artist' ->
-	chooseArtist
+	chooseGenreTrack
 ;	Input = 'year' ->
-	chooseYear
+	chooseGenreTrack
 ; 	write("The input is not a year/artist/genre!\n").
 
+genreOrArtistArtist([Input]) :-
+	Input = 'genre' ->
+	chooseGenreArtist
+;	Input = 'year' ->
+	chooseYearArtist
+; 	write("The input is not a year/artist/genre!\n").
+
+genreOrArtistAlbums([Input]) :-
+	Input = 'genre' ->
+	chooseGenreAlbums
+;	Input = 'artist' ->
+	chooseArtistAlbums
+;	Input = 'year' ->
+	chooseYearAlbums
+; 	write("The input is not a year/artist/genre!\n").
+
+simpleOrNatural([Input]) :-
+	Input = 'g' ->
+	simpleUI
+;	Input = 'n' ->
+	naturalUI
+; 	write("The input is not a year/artist/genre!\n").
+
+trackOrArtistOrAlbum([Input]) :-
+	Input = 'track' ->
+	topTrackUI
+;	Input = 'artist' ->
+	topArtistUI
+;	Input = 'album' ->
+	topAlbumsUI
+; 	write("The input is not a track/artist/album!\n").
+
 % given input of genre calls api and computes genre playlist
-chooseGenre :-
+% For top track
+chooseGenreTrack :-
 	write("Enter a genre(s) (space-separated list):\n"), flush_output(current_output),
 	readln(Input),
 	not(isNumber(Input)) ->
 	atomics_to_string(Input, "+", InputStr),
-	call_api_single(InputStr)
+	call_api_single_track(InputStr)
 ;	write("Sorry! I only accept words\n").
 
 % given input of artist calls api and computes artist playlist
-chooseArtist :-
+chooseArtistTrack :-
 	write("Enter an artist(s) (space-separated list):\n"), flush_output(current_output),
 	readln(Input),
 	not(isNumber(Input)) ->
 	atomics_to_string(Input, "+", InputStr),
-	call_api_single(InputStr)
-;	write("Sorry! I only accept words\n").
+	call_api_single_track(InputStr)
+;	write("Sorry! I only accept numbers\n").
 
 % given input of year calls api and computes year playlist
-chooseYear :-
+chooseYearTrack :-
 	write("Enter a year(s) (space-separated list):\n"), flush_output(current_output),
 	readln(Input),
 	(isNumber(Input)) ->
 	atomics_to_string(Input, "+", InputStr),
-	call_api_single(InputStr)
+	call_api_single_track(InputStr)
 ;	write("Sorry! I only accept words\n").
 
+% For top artist
+chooseGenreArtist :-
+	write("Enter a genre(s) (space-separated list):\n"), flush_output(current_output),
+	readln(Input),
+	not(isNumber(Input)) ->
+	atomics_to_string(Input, "+", InputStr),
+	call_api_single_artist(InputStr)
+;	write("Sorry! I only accept words\n").
 
+% given input of artist calls api and computes artist playlist
+chooseArtistArtist :-
+	write("Enter an artist(s) (space-separated list):\n"), flush_output(current_output),
+	readln(Input),
+	not(isNumber(Input)) ->
+	atomics_to_string(Input, "+", InputStr),
+	call_api_single_artist(InputStr)
+;	write("Sorry! I only accept numbers\n").
+
+% given input of year calls api and computes year playlist
+chooseYearArtist :-
+	write("Enter a year(s) (space-separated list):\n"), flush_output(current_output),
+	readln(Input),
+	(isNumber(Input)) ->
+	atomics_to_string(Input, "+", InputStr),
+	call_api_single_artist(InputStr)
+;	write("Sorry! I only accept words\n").
+
+% For top Albums
+chooseGenreAlbums :-
+	write("Enter a genre(s) (space-separated list):\n"), flush_output(current_output),
+	readln(Input),
+	not(isNumber(Input)) ->
+	atomics_to_string(Input, "+", InputStr),
+	call_api_single_alblum(InputStr)
+;	write("Sorry! I only accept words\n").
+
+% given input of artist calls api and computes artist playlist
+chooseArtistAlbums :-
+	write("Enter an artist(s) (space-separated list):\n"), flush_output(current_output),
+	readln(Input),
+	not(isNumber(Input)) ->
+	atomics_to_string(Input, "+", InputStr),
+	call_api_single_alblum(InputStr)
+;	write("Sorry! I only accept numbers\n").
+
+% given input of year calls api and computes year playlist
+chooseYearAlbums :-
+	write("Enter a year(s) (space-separated list):\n"), flush_output(current_output),
+	readln(Input),
+	(isNumber(Input)) ->
+	atomics_to_string(Input, "+", InputStr),
+	call_api_single_alblum(InputStr)
+;	write("Sorry! I only accept words\n").
 
 % -----USER INTERFACE-----
-simpleUI :-
+mainUI :-
 	write("Welcome to our application -> Song Recommender\n"),
-	write("You can select a genre/artist/year, and I'll return the top 5 songs of that category!\n"),
+	write("Please select the UI -> General User Interface (g), Natural Language User Interface(n)"),
+	readln(Input),
+	( isNumber(Input) ->
+	writeln("Sorry! I only accept words\n")
+;	not(isNaturalOrSimple(Input)) ->
+	writeln("Sorry! That is not a valid option\n") 
+;	simpleOrNatural(Input)
+).
+
+simpleUI :-
+	%write("Welcome to our application -> Song Recommender\n"),
+	write("Please select the category for the tag -> TopTrack (track), TopArtist (artist), TopAlbums (album)"),
+	readln(Input),
+	(isNumber(Input) ->
+	writeln("Sorry! I only accept words\n")
+;   not(isTrackOrArtistOrAlbum(Input)) ->
+	writeln("Sorry! That is not a valid option\n") 
+;	trackOrArtistOrAlbum(Input)
+).
+
+topTrackUI :-
+	write("-------------------------------------------\n"),
+	write("-----------------Top Track-----------------\n"),
+	write("Please also select a \"tag\" for genre/artist/year, and I'll return the top 5 songs of that category!\n"),
     write('Please enter \"genre\", \"artist\", or \"year\": '), flush_output(current_output),
     readln(Input),
 	( isNumber(Input) ->
 	writeln("Sorry! I only accept words\n")
 ;	not(isGenreOrArtist(Input)) ->
 	writeln("Sorry! That is not a valid option\n") 
-;	genreOrArtist(Input)
+;	genreOrArtistTrack(Input)
 ).
+
+
+topArtistUI :-
+	write("-------------------------------------------\n"),
+	write("-----------------Top Artist----------------\n"),
+	write("Please also select a \"tag\" for genre/artist/year, and I'll return the top 5 songs of that category!\n"),
+    write('Please enter \"genre\" or \"year\": '), flush_output(current_output),
+    readln(Input),
+	( isNumber(Input) ->
+	writeln("Sorry! I only accept words\n")
+;	not(isGenreOrArtist(Input)) ->
+	writeln("Sorry! That is not a valid option\n") 
+;	genreOrArtistArtist(Input)
+).
+
+topAlbumsUI :- 
+	write("-------------------------------------------\n"),
+	write("-----------------Top Albums----------------\n"),
+    write("Please also select a \"tag\" for genre/artist/year, and I'll return the top 5 songs of that category!\n"),
+    write('Please enter \"genre\", \"artist\", or \"year\": '), flush_output(current_output),
+    readln(Input),
+	( isNumber(Input) ->
+	writeln("Sorry! I only accept words\n")
+;	not(isGenreOrArtist(Input)) ->
+	writeln("Sorry! That is not a valid option\n") 
+;	genreOrArtistAlbums(Input)
+).
+
 
 % natural Language interface
 % working on it!
 naturalUI :-
-	write("Please enter your question!\n"),
+	%write("Welcome to our application -> Song Recommender\n"),
+	write("Please enter your question! We will analyze your question and return the top 5 songs\n"),
 	readln(Input),
-	Result,
-	parse(Input, Result).
+	parseInput(Input, []).
+% "I like Justin Bieber"
+% "I really like Justin Bieber"
+% "What is the top song of 2021"
+% "What is the top song of 2020"
+% "What is the top album of 2020"
+% "What is the top album of Justin Bieber"
+% "Top album of Justin Bieber"
+% "Who is the top artist of 2020"
+% "I like pop music"
+% "I love pop music"
+% "top song"
+% "Justin Bieber"
+% "pop music"
+% "rock music"
+% "Top album of pop music"
+
+% some of the natural language functions are from the lecture, but I did lots of modification to fit our project
+% A noun phrase is a determiner followed by adjectives followed
+% by a noun followed by an optional modifying phrase:
+noun_phrase(L0,L3) :-
+    det(L0,L1),
+    adjectives(L1,L2),
+    noun(L2,L3).
+
+% parse the input
+parseInput(['Is' | L0],L1) :-
+    noun_phrase(L0,L1).
+parseInput(['What',is | L0],L1) :-
+    noun_phrase(L0,L1).
+parseInput(['What' | L0],L1) :-
+    noun_phrase(L0,L1).
+parseInput(['I', like | L0],L1) :-
+    noun_phrase(L0,L1).
+parseInput(['I', love | L0],L1) :-
+    noun_phrase(L0,L1).
+parseInput(['I', really, like | L0],L1) :-
+    noun_phrase(L0,L1).
+parseInput(['I', really, love | L0],L1) :-
+    noun_phrase(L0,L1).
+parseInput(['Who', is | L0],L1) :-
+    noun_phrase(L0,L1).
+parseInput(['Which', is | L0],L1) :-
+    noun_phrase(L0,L1).
+parseInput(['Which'| L0],L1) :-
+    noun_phrase(L0,L1).
+parseInput(L0,L1) :-
+    noun_phrase(L0,L1).
+
+% Handle the determiners 
+det([the | L],L).
+det([a | L],L).
+det(L,L).
+
+% handle adjective
+adjectives(L0,L2) :-
+    adj(L0,L1),
+    adjectives(L1,L2).
+adjectives(L,L).
+
+% DICTIONARY of adj
+adj([top, song, of | L],[]) :- feature(L, track).
+adj([top, song, in | L],[]) :- feature(L, track).
+adj([top, album, of | L],[]) :- feature(L, album).
+adj([top, album, in | L],[]) :- feature(L, album).
+adj([top, artist, of | L],[]) :- feature(L, artist).
+adj([top, artist, in | L],[]) :- feature(L, artist).
+adj([top| L],L).
+adj(['Top', song, of | L],[]) :- feature(L, track).
+adj(['Top', song, in | L],[]) :- feature(L, track).
+adj(['Top', album, of | L],[]) :- feature(L, album).
+adj(['Top', album, in | L],[]) :- feature(L, album).
+adj(['Top', artist, of | L],[]) :- feature(L, artist).
+adj(['Top', artist, in | L],[]) :- feature(L, artist).
+adj(['Top'| L],L).
+
+noun(song,[]):- feature(topsong).
+noun(songs,[]):- feature(topsong).
+noun(album,[]):- feature(topalbum).
+noun(albums,[]):- feature(topalbum).
+noun(artist,[]):- feature(topartist).
+noun(artists,[]):- feature(topartist).
+noun([X | L],[]) :- append([X],L, Y), feature(Y, general).
+%noun([X | L],L) :- feature(X).
+noun([],[]).
+
+% list of feature
+% default is to search the tag in TopTrack 
+feature(X, general) :- atomics_to_string(X, "+", InputStr), call_api_single_track(InputStr).
+feature(X, track) :- atomics_to_string(X, "+", InputStr), call_api_single_track(InputStr).
+feature(X, album) :- atomics_to_string(X, "+", InputStr), call_api_single_alblum(InputStr).
+feature(X, artist) :- atomics_to_string(X, "+", InputStr), call_api_single_artist(InputStr).
+% default tag is "pop"
+% for top song or album
+feature(topsong) :- call_api_single_track("pop").
+feature(topalbum) :- call_api_single_alblum("pop").
+feature(topartist) :- call_api_single_alblum("pop").
+
 
 
 
