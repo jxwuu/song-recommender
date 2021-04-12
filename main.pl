f3913ed5fd733dcd4aef31f626a3e4297f9a4407
janice <janice.wu.05@gmail.com>
2021-04-11 18:05:04 -0700

    empty output guard

--- main.pl
+++ main.pl
@@ -9,6 +9,9 @@ root_url("http://ws.audioscrobbler.com/2.0").
 api_key("5dd0808d6f467e08e647a024417e7318").
 api_method("tag.gettoptracks").
 possible_tagArray([genre, artist, year]).
+possible_genre([pop,hiphop,rock,swing,jazz,blues,classical,poprock,punkrock,emo,screamo,folk,indie,edm,electronic,techno,synth,rap,metal,
+				heavymetal,alt,alternative,funk,techno,soul,dance,house,reggae,world,gospel,kpop,grunge,ambient,opera,hardrock,orchestra,ballad,soundtrack,dubstep
+				,country, countryrock,industrial,psychedelic]).
 
 
 
@@ -82,8 +85,10 @@ make_api_call(URL, Songs) :-
  	json_read_dict(In_stream, Dict),
  	close(In_stream),
  	json_to_songs(Dict, Songs),
-	write_songnames(Songs),
- 	write("\n").
+	(ifEmpty(Songs) ->
+	write("Sorry! There were no songs found!")
+	;write_songnames(Songs),
+ 	write("\n")).
 
 % builds the URL and makes the API call for this particular Item
 call_api_single(Item) :-
@@ -106,21 +111,15 @@ isGenreOrArtist([In]) :-
 	possible_tagArray(P),
 	member(In,P).
 
-%% % verifies if Input is the atom 'genre'
-%% ifGenre(Input) :-
-%% 	Input = 'genre'.
+% verfies if input is artist
+ ifEmpty(Songs) :-
+ 	Songs = [].
 
-%% % verfies if input is year
-%% ifYear(Input) :-
-%% 	Input = 'year'.
-
-%% % verfies if input is artist
-%% ifArtist(Input) :-
-%% 	Input = 'artist'.
-
-%% % verfies if input is artist
-%% ifEmpty(Songs) :-
-%% 	Songs = [].
+% verifies possible year input
+isPossibleYear([Input]) :-
+	number(Input),
+	Input < 2022,
+	Input > 1800.
 
 % if input is genre then computes genre playlist
 genreOrArtist([Input]) :-
@@ -154,10 +153,10 @@ chooseArtist :-
 chooseYear :-
 	write("Enter a year(s) (space-separated list):\n"), flush_output(current_output),
 	readln(Input),
-	(isNumber(Input)) ->
-	atomics_to_string(Input, "+", InputStr),
+	(isPossibleYear(Input)) ->
+	atomics_to_string(Input,InputStr),
 	call_api_single(InputStr)
-;	write("Sorry! I only accept words\n").
+;	write("Sorry! That is not a valid input!\n").
 
 
 
